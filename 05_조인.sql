6fcc1060097f68103a608b2848b2e2cfe6515542
LJO <liv01080208@gmail.com>
2021-10-07 09:01:22 +0900

    1006 수업

---
+++ 05_조인.sql
@@ -0,0 +1,30 @@
+-- 조인(JOIN)
+
+-- ON을 사용한 기본 조인
+SELECT E.EMPLOYEE_ID 직원번호, E.LAST_NAME 이름, D.DEPARTMENT_ID 부서번호, D.DEPARTMENT_NAME 부서이름
+FROM EMPLOYEES E JOIN DEPARTMENTS D
+    ON E.DEPARTMENT_ID = D.DEPARTMENT_ID;
+    
+-- WHERE 절을 사용한 예전 조인방법
+SELECT E.EMPLOYEE_ID 직원번호, E.LAST_NAME 이름, D.DEPARTMENT_ID 부서번호, D.DEPARTMENT_NAME 부서이름
+FROM EMPLOYEES E, DEPARTMENTS D
+WHERE E.DEPARTMENT_ID = D.DEPARTMENT_ID;
+
+-- 3개 테이블 직원 부서 로케이션 테이블 조인하기
+SELECT E.EMPLOYEE_ID 직원번호, D.DEPARTMENT_NAME 부서이름, L.CITY 도시
+FROM EMPLOYEES E
+JOIN DEPARTMENTS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+JOIN LOCATIONS L ON L.LOCATION_ID = D.LOCATION_ID;
+
+-- 예제 다시 확인하기
+SELECT D.department_name 부서명, L.city 도서, C.country_name 국가
+FROM DEPARTMENTS D
+JOIN LOCATIONS L ON D.LOCATION_ID = L.LOCATION_ID
+JOIN COUNTRIES D ON L.CONUTRY_ID =  C.COUNTRY_ID
+WHERE L.CITY IN('Seattle', 'London') AND C.COUNTRY_NAME LIKE 'United%';
+
+-- 자체 조인
+select e.last_name 직원, m.last_name 매니저
+from employees e
+join employees m
+on e.manager_id = m.employee_id;
